431531207ffa587943c5796eae92713a3d04d8d4
Gand46 <g.andres4686@gmail.com>
2026-05-30 15:38:42 -0500

    Update binaries and project requirements

---
+++ Builds/Launchers/megajava.bat
@@ -0,0 +1,8 @@
+@echo off
+title MegaBurner
+
+cd /d "D:\CONSOLAS\Wonderswan\MegaBurner"
+
+"C:\Program Files (x86)\Eclipse Adoptium\jdk-8.0.462.8-hotspot\bin\java.exe" -Djava.library.path=. -jar "MegaBurner.jar"
+
+pause
